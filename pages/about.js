4c5433f9cdd3c85f078f7b13e726a296efbb8d6f
zachvenables <venables.3@buckeyemail.osu.edu>
2020-12-12 15:38:59 -0500

    latest update

--- pages/about.js
+++ pages/about.js
@@ -22,9 +22,9 @@ class About extends React.Component{
 
                     <div className={styles.topnav}>
 				        <a href="/">Home</a>
-				        <a className={styles.active} href="/donate">About Us</a>
+				        <a className={styles.active} href="/about">About Us</a>
 				        <a href="/events">Events</a>
-				        <a href="/about">Donate</a>
+				        <a href="/donate">Donate</a>
 			        </div>
            
                     <img src="https://fixcom.azureedge.net/assets/content/15713/pantry-power-header.jpg" className={styles.image}/>
@@ -32,9 +32,12 @@ class About extends React.Component{
                     <div className={styles.grid}>
                       <a href="/donate" className={styles.card}>
                         <h3>Donate Today</h3>
-                        <p></p>
                       </a>
                     </div>
+
+                    <img className={styles.aboutImg} src="https://i.imgur.com/KAo2O4r.jpg"/>
+                    <h2 className={styles.announcements}>Our Story</h2>
+                    <p className={styles.description}>In 1958, retired Reynoldsburg School nurse, Mary Frances, decided to give aid to those students and families she had served for many years. Along with some local help, she began Reynoldsburg Helping Hands food pantry to benefit those that lived in the Reynoldsburg School District. Today, Reynoldsburg Helping Hands has grown to serve individuals and families living within the entire City of Reynoldsburg. Over 20 volunteers give hours of their time each week shopping for food, sorting donations, stocking shelves, making appointments, and packing food for clients. Reynoldsburg Helping Hands is committed to serving their community and providing assistance to those in need.</p>
 
                   </main>
 
--- pages/events.js
+++ pages/events.js
@@ -36,6 +36,11 @@ class Events extends React.Component{
                       </a>
                     </div>
 
+                    
+                    <img src="https://i.imgur.com/Z0Raoax.jpg"  className={styles.golfSponsors} />
+                    <p className={styles.description}>The Chuck Martin Memorial Golf Outing is an event that raises money every year to donate to charities in the community. In the summer of 2020, the Martin Family chose to donate proceeds from the golf outing to support Reynoldsburg Helping Hands. After collaborating with the food pantry's volunteers to gain sponsors and organize prizes for the outing, the culminating event on September 13th, 2020 saw 18 groups participate in the golf outing and ended with a raffle for donated prizes. Thank you to the Martin Family and all the participants and local sponsors for making this event fun and extremely successful!</p>
+                    
+
                   </main>
 
                   <footer className={styles.footer}>
