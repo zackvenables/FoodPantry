e65e158177042c1310c3cca2f5c01d539f272475
Zachary Venables <venables.3@buckeyemail.osu.edu>
2023-11-10 07:09:25 -0500

    updates nov 23

--- pages/events.js
+++ pages/events.js
@@ -9,6 +9,15 @@ class Events extends React.Component{
 	render(){
 		return(
       <>
+      <img src="https://i.imgur.com/58MtYQT.jpg" ></img>
+      <div>
+        
+      </div>
+      <img src="https://i.imgur.com/7o4hkMl.jpg" ></img>
+       <p className={styles.description}>The "Battle of 256" took place on Friday, Oct. 20th. Reynoldsburg competed against Pckerington Central in a high school football game as well as a food drive for their local communities. Reynoldsburg Helping Hands would like to thank BEAR, a Reynoldsburg-based organization, for leading this initiative and giving back to their community!</p>
+        <div>
+
+        </div>
         <img src="https://i.imgur.com/Z0Raoax.jpg"  className={styles.golfSponsors} />
         <img src="https://i.imgur.com/KINQW3d.png" className={styles.golfFamily}/>
         <p className={styles.description}>The Chuck Martin Memorial Golf Outing is an event that raises money every year to donate to charities in the community. In the summer of 2020, the Martin Family chose to donate proceeds from the golf outing to support Reynoldsburg Helping Hands. After collaborating with the food pantry's volunteers to gain sponsors and organize prizes for the outing, the culminating event on September 13th, 2020 saw 18 groups participate in the golf outing and ended with a raffle for donated prizes. Thank you to the Martin Family and all the participants and local sponsors for making this event fun and extremely successful!</p>
--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,22 @@ class Home extends React.Component{
       <><>
         <h2 className={styles.announcements}>Announcements</h2>
         <p className={styles.description}>Save the date! The next food drive will be Saturday, Nov. 11th, 10:00am - 2:00pm. Please give what you can to support your local community - we are in need of non-perishable food items, toiletries, and household items such as laundry detergent.</p>
+        <div>
+
+        </div>
+        <h4 className={styles.assistanceTitles}>Upcoming Closures: </h4><p className={styles.description}> Reynoldsburg Helping Hands will be closed Saturday, Nov. 11th for the food drive , Friday, Nov. 23rd and the week of Dec. 25th - Jan. 1st in observance of the upcoming holidays. </p>
+        <div>
+          
+        </div>
       <h2 className={styles.description}>Click <a className="yodel" href="https://events.yodel.today/reynoldsburg">here</a> to see other organizations in Reynoldsburg and make sure to "follow" Reynoldsburg Helping Hands to keep up to date on upcoming news and events! </h2>
       <div>
 
       </div>
-      <h2 className={styles.announcements}>For Assistance</h2><h4 className={styles.assistanceTitles}>Hours of Operation: </h4><p className={styles.description}>Tuesday, Friday, Saturday from 10:00AM - Noon, Wednesday 4:00 - 6:00PM</p><ul className={styles.list}>
+      <h2 className={styles.announcements}>For Assistance</h2><h4 className={styles.assistanceTitles}>Hours of Operation: </h4><p className={styles.description}>Tuesday, Friday, Saturday from 10:00AM - Noon, Wednesday 3:00 - 5:00PM</p><ul className={styles.list}>
+          
+        <div>
+
+        </div>
           <li>It is necessary to call a day ahead for an appointment.</li>
           <li>Identification and proof of address are required when picking up groceries.</li>
           <li>One call per address. Please be on time for your appointment.</li>
